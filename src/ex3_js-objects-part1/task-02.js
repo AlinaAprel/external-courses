a5d4ff4b8640741c3c620debe7888f9e7e493d8f
AlinaAprel <68186607+AlinaAprel@users.noreply.github.com>
2021-11-15 15:54:58 +0300

    Delete task-02.js

--- src/ex3_js-objects-part1/task-02.js
+++
@@ -1,8 +0,0 @@
-function seeKeys(obj) {
-  const keys = Object.keys(obj);
-  const values = Object.values(obj);
-  console.log(keys, values);
-  return `Ключи: ${keys}, значения свойств: ${values}`;
-}
-
-module.exports = seeKeys;
