cda67e483ef52054c994a405b3e85bbafeb0a8ad
Alina Aprel <alina.aprelh@mail.ru>
2021-12-06 15:32:02 +0300

    delete another tasks

--- src/ex3_js-objects-part1/task-03.js
+++
@@ -1,9 +0,0 @@
-function doYouHaveAProperty(str, obj) {
-  let result = false;
-  if (Object.keys(obj).length !== 0) {
-    result = obj.hasOwnProperty(str);
-  }
-  return result;
-}
-
-module.exports = doYouHaveAProperty;
