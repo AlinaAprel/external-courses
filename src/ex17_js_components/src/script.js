f7199030bbd5bac7b770a65626823b300876974c
Alina Aprel <alina.aprelh@mail.ru>
2021-12-14 12:33:07 +0300

    fix normalize and script

--- src/ex17_js_components/src/script.js
+++ src/ex17_js_components/src/script.js
@@ -6,29 +6,27 @@ import { boardsMocks } from './components/boardsMocks';
 
 /* eslint-disable no-undef */
 // eslint-disable-next-line func-names
-(function () {
-  const tasksList = document.querySelector('.tasks-list');
-  const account = document.querySelector('.header__account');
-  const btnArrow = document.querySelector('.header__arrow');
-  const menu = document.querySelector('.menu');
+const tasksList = document.querySelector('.tasks-list');
+const account = document.querySelector('.header__account');
+const btnArrow = document.querySelector('.header__arrow');
+const menu = document.querySelector('.menu');
 
-  const data = new Data(boardsMocks);
-  const taskList = new TaskList(tasksList, boardsMocks);
-  data.setData();
-  taskList.render();
+const data = new Data(boardsMocks);
+const taskList = new TaskList(tasksList, boardsMocks);
+data.setData();
+taskList.render();
 
-  const userMenu = new UserMenu(menu, btnArrow);
-  const buttons = document.querySelectorAll('.tasks-list__button');
-  const task = new Task(buttons, data);
-  task.setEventListeners();
+const userMenu = new UserMenu(menu, btnArrow);
+const buttons = document.querySelectorAll('.tasks-list__button');
+const task = new Task(buttons, data);
+task.setEventListeners();
 
-  account.addEventListener('click', () => {
-    if (document.querySelector('.menu__list')) {
-      userMenu.close();
-      userMenu.btnUp();
-    } else {
-      userMenu.createMenu();
-      userMenu.btnDown();
-    }
-  });
-}());
+account.addEventListener('click', () => {
+  if (document.querySelector('.menu__list')) {
+    userMenu.close();
+    userMenu.btnUp();
+  } else {
+    userMenu.createMenu();
+    userMenu.btnDown();
+  }
+});
